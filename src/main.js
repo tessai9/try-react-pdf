57c6bd1e5499ae787a594a51716450a382e9c2e8
rikusen0335 <rikusen0335@gmail.com>
2021-09-27 17:48:06 +0900

    必要のないrequireを削除

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
-require("regenerator-runtime/runtime");
-
 ReactDOM.render(
   <App />,
   document.getElementById('app')
